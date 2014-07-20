56a94b30378df6877bebe4c6d6f6a430a37f30cf
Cameron Leslie <cameronleslie@gmail.com>
2014-07-20 14:24:21 -0400

    Adding Timer and abandon functionality

--- curtain.js
+++ curtain.js
@@ -6,6 +6,7 @@ var CURTAIN = (function () {
   //Public Vars
   curtain.hidden = '';
   curtain.visibilityChange = '';
+  curtain.hiddenInterval = '';
   //Public Functions
   //Check browser for API variations.
   curtain.checkBrowser = function () {
@@ -30,6 +31,11 @@ var CURTAIN = (function () {
   //Fires on hidden state, changes css transistions and Z-index.
   //This is initial state that the shit transistions from.
   curtain.hiddenState = function () {
+    //Checking to see if the countdown is running
+    if (curtain.hiddenInterval) {// clear that shit.
+      clearInterval(curtain.hiddenInterval);
+      curtain.hiddenInterval = '';
+    }
     $('.curtain').css({
       'transition': 'all 0s',
       'transition-delay': '0s',
@@ -51,12 +57,48 @@ var CURTAIN = (function () {
       });
     }, 10);
   };
+  //clone and delete original. We need to do this because you can't just "interrupt"
+  //css transistions, you have to actually remove/clone the WHOLE element. Shitty.
+  //Makes sense I guess. Kinna. Not really. Maybe. I have to put the sausage and cheese cart back.
+  curtain.cloneElement = function () {
+    var cur = $('.curtain'),
+    newcur = cur.clone(true);
+    cur.before(newcur);
+    $("." + cur.attr("class") + ":last").remove();
+  };
+  //bind the & jquery click even to the exit.
+  curtain.startExit = function () {
+    $('.curtain-exit').click( function () {
+      var settings = curtain.getSettings();
+      curtain.cloneElement();
+      curtain.hiddenState();
+      curtain.wipe('0',settings[1]);
+    });
+  };
+  // Countdown Timer, in seconds
+  curtain.countdown = function (counttime) {
+    //stript text from the numbers.
+    var re = /[^\d\.]/;
+    var count = counttime.replace(re,'');
+    curtain.hiddenInterval = setInterval(function() {
+      count--;
+      if (count >= 0) {
+        $('.curtain-count').html(count);
+      }
+      if (count === 0) {
+        $('.curtain-count').html('');
+      }
+    }, 1000);
+  }
   //Lets update the css via the event listener.
   curtain.wipe = function (delay, duration) {
     if (document[curtain.hidden]) {//Is the DOM hidden? Are you on another Tab?
       curtain.hiddenState();
+
     } else { // DOM is shown. We can see it!
+      $('.curtain-count').html('');//clear the counter.
       curtain.returnState(delay,duration);
+      curtain.countdown(delay);
     }
   };
   //Grabs the settings from the DOM using $'s attr. function.
@@ -70,6 +112,7 @@ var CURTAIN = (function () {
   curtain.init = function (){
     //Fire the first check
     curtain.checkBrowser();
+    curtain.startExit();
     //Attach Listener
     document.addEventListener(curtain.visibilityChange, function () {//Damn you Javascript. THANKS, OBAMA./s
       //Get settings from the Dom.
@@ -80,5 +123,4 @@ var CURTAIN = (function () {
   //Push that shit out the door.
   return curtain;
 }());
-
 CURTAIN.init();
